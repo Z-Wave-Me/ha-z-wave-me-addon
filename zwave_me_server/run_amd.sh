399af4a647bcf998fd0060cc89ccd9a81a068cca
msazanov <msazanov@gmail.com>
2022-12-14 00:21:57 +0400

    modified:   Dockerfile.aarch64 	modified:   run_amd.sh 	modified:   run_arm.sh

--- zwave_me_server/run_amd.sh
+++ zwave_me_server/run_amd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /etc/init.d/z-way-server start
-tail --pid `cat /var/run/z-way-server.pid` -n +1 -F /var/log/z-way-server.log
+tail --pid `cat /var/run/z-way-server.pid` -n +1 -F /var/log/z-way-server.log | /opt/z-way-server/colorize-log.sh
