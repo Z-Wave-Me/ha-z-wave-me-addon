bd13d06e24f99d47694ea7918660dc99f05e8cd3
msazanov <msazanov@gmail.com>
2022-12-13 23:41:19 +0400

    modified:   run_amd.sh 	modified:   run_arm.sh

--- zwave_me_server/run_amd.sh
+++ zwave_me_server/run_amd.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 /etc/init.d/z-way-server start
 tail --pid `cat /var/run/z-way-server.pid` -n +1 -F /var/log/z-way-server.log
--- zwave_me_server/run_arm.sh
+++ zwave_me_server/run_arm.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 /etc/init.d/zbw_connect start
 /etc/init.d/mongoose start
 /etc/init.d/z-way-server start
 
-tail --pid `cat /var/run/z-way-server.pid` -n +1 -F /var/log/z-way-server.log | /opt/z-way-server/ha-colorize-log.sh
+tail --pid `cat /var/run/z-way-server.pid` -n +1 -F /var/log/z-way-server.log
