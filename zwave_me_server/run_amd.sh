1d5906c0a35d528701f68a15a9e296bdb48113e1
msazanov <msazanov@gmail.com>
2022-12-16 15:35:19 +0400

    Fix save addon data, add device selector, fix images 	modified:   zwave_me_server/Dockerfile Add defauild time zone for making image, and remove VOLUME 	modified:   zwave_me_server/Dockerfile.aarch64 Remove VOLUME 	modified:   zwave_me_server/Dockerfile.armhf Remove VOLUME 	modified:   zwave_me_server/config.yaml Add device selector and remove volume 	modified:   zwave_me_server/run_amd.sh Add colorize log and symlinks to external static /data folder 	modified:   zwave_me_server/run_arm.sh Add colorize log and symlinks to external static /data folder

--- zwave_me_server/run_amd.sh
+++ zwave_me_server/run_amd.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+
+#make storage folders
+mkdir -p /data/z-way-server/automation/storage/
+mkdir -p /data/z-way-server/config/zddx/
+mkdir -p /data/z-way-server/automation/userModules/
+
+#save data to /data/z-way-server
+rm -rf /opt/z-way-server/automation/storage/ && 	ln -s /data/z-way-server/automation/storage/ /opt/z-way-server/automation/
+rm -rf /opt/z-way-server/config/zddx/ && 			ln -s /data/z-way-server/config/zddx/ /opt/z-way-server/config/
+rm -rf /opt/z-way-server/automation/userModules/ && ln -s /data/z-way-server/automation/userModules/ /opt/z-way-server/automation/
+
+
 /etc/init.d/z-way-server start
-tail --pid `cat /var/run/z-way-server.pid` -F /var/log/z-way-server.log
+tail --pid `cat /var/run/z-way-server.pid` -F /var/log/z-way-server.log | /opt/z-way-server/colorize-log.sh
