d3a8447d71df6bfe1b19e5ef943ea0719af491e9
Poltorak Serguei <poltorak@alsenet.com>
2022-12-23 20:09:58 +0300

    Update run_arm.sh

--- zwave_me_server/run_arm.sh
+++ zwave_me_server/run_arm.sh
@@ -8,10 +8,6 @@ ln -s $d /dev/zwave
 # change device path in /defailtConfig/config.json
 sed -Ei "s|/dev/tty[A-Z]*[0-9]|/dev/zwave|g" /opt/z-way-server/automation/defaultConfigs/config.json
 
-echo -n "Selected device in configjson: "  ; grep -Eo '\/dev\/tty[A-z]*[0-9]' $cj
-echo -n "Selected device in config.json: " ; grep -Eo '\/dev\/tty[A-z]*[0-9]' /opt/z-way-server/automation/defaultConfigs/config.json
-echo -n "Selected device in options.json: "; grep -Eo '\/dev\/tty[A-z]*[0-9]' /data/options.json
-
 # copy files the first boot and link paths to /data on each boot
 for path in /opt/z-way-server/automation/storage/ /opt/z-way-server/config/zddx/ /opt/z-way-server/automation/userModules/ /etc/init.d/ /etc/z-way/ /etc/zbw/; do
   if [ -f $path -a ! -f /data/$path ]; then
